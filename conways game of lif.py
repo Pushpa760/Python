37a554944b9b832141c5a78055c74c923f4aa5d2
Pushpa760 <pushpass0331@gmail.com>
2025-05-21 20:25:17 +0530

    Add files via upload

---
+++ conways game of lif.py
@@ -0,0 +1,23 @@
+print("name=pushpa,usn=1AY24Ai086,section=O")
+import time
+import os
+
+# Define grid size
+WIDTH = 10
+HEIGHT = 10
+
+# Create an initial grid (0 = dead, 1 = alive)
+grid = [[0]*WIDTH for _ in range(HEIGHT)]
+
+# Add a simple pattern (a "blinker")
+grid[4][4] = 1
+grid[4][5] = 1
+grid[4][6] = 1
+
+def print_grid(grid):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    for row in grid:
+        for cell in row:
+            print('■' if cell else ' ', end='')
+        print()
+print()
