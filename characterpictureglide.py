b38ea18c11f2750f2c8a286c4cf2d5eb0d58e504
Pushpa760 <pushpass0331@gmail.com>
2025-05-25 18:22:19 +0530

    Add files via upload

---
+++ characterpictureglide.py
@@ -0,0 +1,17 @@
+print("name=pushpa,usn=1AY24AI086,section=O")
+grid = [
+    ['.', '.', '.', '.', '.', '.'],
+    ['.', 'O', 'O', '.', '.', '.'],
+    ['O', 'O', 'O', 'O', '.', '.'],
+    ['O', 'O', 'O', 'O', 'O', '.'],
+    ['.', 'O', 'O', 'O', 'O', 'O'],
+    ['O', 'O', 'O', 'O', 'O', '.'],
+    ['O', 'O', 'O', 'O', '.', '.'],
+    ['.', 'O', 'O', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.']
+]
+
+for x in range(len(grid[0])):  # loop over columns
+    for y in range(len(grid)):  # loop over rows
+        print(grid[y][x], end='')
+print()
