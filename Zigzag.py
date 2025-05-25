b770cfcc15576a2f9b71f6d69789f7bdaf3e1300
Pushpa760 <pushpass0331@gmail.com>
2025-05-25 19:10:21 +0530

    Update Zigzag.py

--- Zigzag.py
+++ Zigzag.py
@@ -2,18 +2,18 @@ print("name=pushpa,usn=1AY24AI086,section=O")
 import time
 import sys
 
-def zigzag():
+def zigzag(steps):
     indent = 0
     indent_increasing = True
 
     try:
-        while True:
+        for _ in range(steps):
             print(' ' * indent + '*')
             time.sleep(0.1)
 
             if indent_increasing:
                 indent += 1
-                if indent == 20:
+                if indent == 5:
                     indent_increasing = False
             else:
                 indent -= 1
@@ -22,4 +22,4 @@ def zigzag():
     except KeyboardInterrupt:
         sys.exit("\nZigzag animation stopped.")
 
-zigzag()
+zigzag(10)
